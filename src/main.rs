ff04a6a06fc59b39ec9a9b2dd0d0a202927f05cf
Louis Thevenet <louis.tvnt@gmail.com>
2024-05-06 21:11:43 +0200

    feat: simulation plugin

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
+use ai_snake::simulation::AISnakePlugin;
 use bevy::{app::App, DefaultPlugins};
 use snake_game::game::SnakeGamePlugin;
+mod ai_snake;
 mod rendering;
 mod snake_core;
 mod snake_game;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugins(SnakeGamePlugin)
+        .add_plugins(AISnakePlugin)
         .run();
 }
